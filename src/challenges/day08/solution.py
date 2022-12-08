b1b2fd898b2eb18310a0eeb02fb98a1c5e53e28f
francesco monti <francesco.monti@cleafy.com>
2022-12-08 18:36:01 +0100

    🎅🏻 add second challenge solution

--- src/challenges/day08/solution.py
+++ src/challenges/day08/solution.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 from src.challenges import read_input_removing_new_line
 from tree import Tree
@@ -10,14 +11,7 @@ tree_grid_df = pd.DataFrame(tree_grid_rows_int)
 tree_grid_df = tree_grid_df.applymap(lambda elem: Tree(elem))
 
 
-def mark_tree_visible(tree_grid: pd.DataFrame, tree: Tree, tree_row, tree_column):
-    column = tree_grid[tree_column]
-    row = tree_grid.iloc[tree_row]
-    top_trees = column[:tree_row]
-    bottom_trees = column[tree_row + 1:]
-    left_trees = row[:tree_column]
-    right_trees = row[tree_column + 1:]
-
+def mark_tree_visible(tree: Tree, top_trees, bottom_trees, left_trees, right_trees):
     top_trees_mask = [tree.is_higher(other_tree) for other_tree in top_trees]
     bottom_trees_mask = [tree.is_higher(other_tree) for other_tree in bottom_trees]
     left_trees_mask = [tree.is_higher(other_tree) for other_tree in left_trees]
@@ -26,15 +20,55 @@ def mark_tree_visible(tree_grid: pd.DataFrame, tree: Tree, tree_row, tree_column
     tree.visible = all(top_trees_mask) or all(bottom_trees_mask) or all(left_trees_mask) or all(right_trees_mask)
 
 
-tree_grid_matrix = tree_grid_df.to_numpy().tolist()
+def get_top_bottom_left_right_trees(tree_grid: pd.DataFrame, tree_row, tree_column):
+    column = tree_grid[tree_column]
+    row = tree_grid.iloc[tree_row]
+    top_trees = column[:tree_row]
+    bottom_trees = column[tree_row + 1:]
+    left_trees = row[:tree_column]
+    right_trees = row[tree_column + 1:]
+    return top_trees, bottom_trees, left_trees, right_trees
+
+
+def get_visible_trees(tree: Tree, top_trees, bottom_trees, left_trees, right_trees):
+    top_trees = np.flip(top_trees.to_numpy())
+    left_trees = np.flip(left_trees.to_numpy())
+    tree.top_visible = get_visible_trees_count_one_side(tree, top_trees)
+    tree.bottom_visible = get_visible_trees_count_one_side(tree, bottom_trees)
+    tree.left_visible = get_visible_trees_count_one_side(tree, left_trees)
+    tree.right_visible = get_visible_trees_count_one_side(tree, right_trees)
+
+
+def get_visible_trees_count_one_side(tree: Tree, other_trees_list):
+    if len(other_trees_list) == 0:
+        return 0
+    visible_trees = 0
+    for other_tree in other_trees_list:
+        if other_tree.is_shorter(tree):
+            visible_trees += 1
+        else:
+            visible_trees += 1
+            break
+    return visible_trees
+
+
+tree_grid_matrix: list = tree_grid_df.to_numpy().tolist()
 
 for row_index in range(len(tree_grid_matrix)):
     for column_index in range(len(tree_grid_matrix[row_index])):
         tree_to_analyze = tree_grid_matrix[row_index][column_index]
-        mark_tree_visible(tree_grid_df, tree_to_analyze, row_index, column_index)
+        top_trees, bottom_trees, left_trees, right_trees = \
+            get_top_bottom_left_right_trees(tree_grid_df, row_index, column_index)
+        mark_tree_visible(tree_to_analyze, top_trees, bottom_trees, left_trees, right_trees)
+        get_visible_trees(tree_to_analyze, top_trees, bottom_trees, left_trees, right_trees)
 
 visible_trees_mask = tree_grid_df.applymap(lambda tree: tree.visible)
 visible_tree_count = sum(map(sum, visible_trees_mask.to_numpy().tolist()))
 
 print(f'first solution: {visible_tree_count}')
-a = 1
+
+scenic_score_map = tree_grid_df.applymap(lambda tree: tree.compute_scenic_score())
+
+max_scenic_score = scenic_score_map.max().max()
+
+print(f'second solution: {max_scenic_score}')
--- src/challenges/day08/tree.py
+++ src/challenges/day08/tree.py
@@ -2,9 +2,19 @@ class Tree:
     def __init__(self, height):
         self.height = height
         self.visible = False
+        self.left_visible = 0
+        self.right_visible = 0
+        self.top_visible = 0
+        self.bottom_visible = 0
 
     def __str__(self):
         return f'{self.height} - {self.visible}'
 
     def is_higher(self, other_tree):
         return self.height > other_tree.height
+
+    def is_shorter(self, other_tree):
+        return self.height < other_tree.height
+
+    def compute_scenic_score(self):
+        return self.left_visible * self.right_visible * self.top_visible * self.bottom_visible
