510f22187a1e63329f50a9a06dcbc37f89ceab1a
Framonti <francesco.monti3@gmail.com>
2022-12-12 14:21:01 +0100

    🎅 add second part solution

--- src/challenges/day12/solution.py
+++ src/challenges/day12/solution.py
@@ -1,6 +1,7 @@
 import string
 import networkx as nx
 import numpy as np
+from networkx import NetworkXNoPath
 
 from src.challenges import read_input_removing_new_line
 from grid_network import GridNetwork
@@ -21,6 +22,28 @@ input_grid[ending_point] = 26
 input_grid_numeric = input_grid.astype(int)
 grid_network = GridNetwork(input_grid_numeric, starting_point, ending_point)
 
-shortest_distance = nx.dijkstra_path(grid_network.graph, grid_network.starting_position, grid_network.ending_position, weight='weight')
+shortest_distance = nx.dijkstra_path(grid_network.graph, grid_network.starting_position, grid_network.ending_position,
+                                     weight='weight')
 
 print(f'first solution: {len(shortest_distance) - 1}')
+
+starting_points_list = np.where(input_grid_numeric == 1)
+
+starting_points = []
+for i in range(len(starting_points_list[0])):
+    starting_points.append([starting_points_list[0][i], starting_points_list[1][i]])
+
+
+def compute_path(starting_point):
+    ARBITRARY_LONG_DISTANCE = 10_000
+    try:
+        distance = len(nx.dijkstra_path(grid_network.graph, f'{starting_point[0]}, {starting_point[1]}',
+                                    grid_network.ending_position, weight='weight')) - 1
+    except NetworkXNoPath:
+        distance = ARBITRARY_LONG_DISTANCE
+    return distance
+
+
+all_distances = [compute_path(starting_point) for starting_point in starting_points]
+
+print(f'second solution: {min(all_distances)}')
