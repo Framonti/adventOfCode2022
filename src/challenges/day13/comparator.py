a7e89da3fec150a58a081fccb50caae9cb5fc9dc
Framonti <francesco.monti3@gmail.com>
2022-12-13 18:36:44 +0100

    🧹 partial cleaning using match

--- src/challenges/day13/comparator.py
+++ src/challenges/day13/comparator.py
@@ -1,4 +1,5 @@
 import json
+import math
 from functools import total_ordering
 
 
@@ -9,33 +10,23 @@ def compare_pair(left, right, recursive=False):
         elif i == len(right) and len(right) < len(left):
             return 1
 
-        # both integers
-        if isinstance(left[i], int) and isinstance(right[i], int):
-            if left[i] < right[i]:
-                return -1
-            elif left[i] > right[i]:
-                return 1
-            else:
-                continue
-
-        # both lists
-        elif isinstance(left[i], list) and isinstance(right[i], list):
-            recursive_result = compare_pair(left[i], right[i], recursive=True)
-            if not isinstance(recursive_result, tuple):
-                return recursive_result
-        elif isinstance(left[i], list) and isinstance(right[i], int):
-            converted = [right[i]]
-            recursive_result = compare_pair(left[i], converted, recursive=True)
-            if not isinstance(recursive_result, tuple):
-                return recursive_result
-        else:
-            converted = [left[i]]
-            recursive_result = compare_pair(converted, right[i], recursive=True)
-            if not isinstance(recursive_result, tuple):
-                return recursive_result
-
+        match left[i], right[i]:
+            case int(), int():
+                if left[i] == right[i]:
+                    continue
+                return math.copysign(1, left[i] - right[i])
+            case list(), list():
+                recursive_result = compare_pair(left[i], right[i], recursive=True)
+                if not recursive_result == 'still_checking':
+                    return recursive_result
+            case list(), int():
+                return compare_pair(left[i], [right[i]])
+            case int(), list():
+                recursive_result = compare_pair([left[i]], right[i], recursive=True)
+                if not recursive_result == 'still_checking':
+                    return recursive_result
     if recursive:
-        return 1, 'still_checking'
+        return 'still_checking'
     return 1
 
 
